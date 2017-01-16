44194835d0c61efb51320b14b5e6c436d7e3ee23
Tomas Valenta <semanticer@gmail.com>
2017-01-16 16:25:30 +0100

    Improve visual from numebrs to circles

--- app/src/main/java/com/example/semanticer/unstable/presentation/BoardFieldWidget.java
+++ app/src/main/java/com/example/semanticer/unstable/presentation/BoardFieldWidget.java
@@ -23,9 +23,12 @@ import com.example.semanticer.unstable.domain.model.Player;
 public class BoardFieldWidget extends View {
 
     private TextPaint textPaint;
+    private Paint circlePaint;
+    private Paint gridPaint;
     private GameField gameField;
     private int row;
     private int column;
+    private float marginSize;
 
     public BoardFieldWidget(Context context, GameField gameField, int row, int column) {
         super(context);
@@ -43,6 +46,12 @@ public class BoardFieldWidget extends View {
         textPaint.setTextSize(40);
         textPaint.setColor(gameField.player() == Player.ANON ? Color.BLACK : gameField.player() == Player.FIRST_PLAYER ? Color.RED : Color.BLUE);
 
+        circlePaint = new Paint();
+        circlePaint.setColor(gameField.player() == Player.ANON ? Color.BLACK : gameField.player() == Player.FIRST_PLAYER ? Color.RED : Color.BLUE);
+
+        gridPaint = new Paint();
+        gridPaint.setColor(Color.BLACK);
+
         int[] attrs = new int[]{R.attr.selectableItemBackground};
         TypedArray typedArray = getContext().obtainStyledAttributes(attrs);
         int backgroundResource = typedArray.getResourceId(0, 0);
@@ -50,6 +59,7 @@ public class BoardFieldWidget extends View {
         typedArray.recycle();
 
         float fieldSize = getResources().getDimension(R.dimen.field_size);
+        marginSize = getResources().getDimension(R.dimen.field_margin_size);
         GridLayout.LayoutParams param = new GridLayout.LayoutParams();
         param.height = (int) fieldSize;
         param.width = (int) fieldSize;
@@ -69,7 +79,26 @@ public class BoardFieldWidget extends View {
 
         int width = getWidth();
         int height = getHeight();
-        canvas.drawText(gameField.atomCount() + "", width/2, height/2, textPaint);
+
+
+        int atomWidth = width / 3;
+
+        canvas.drawRect(0, 0, width, height, gridPaint);
+
+        if (gameField.atomCount() == 1) {
+            canvas.drawOval((width/2)-(atomWidth/2), (height/2)-(atomWidth/2), (width/2)+(atomWidth/2), (height/2)+(atomWidth/2), circlePaint);
+        } else if (gameField.atomCount() == 2){
+            canvas.drawOval(marginSize, marginSize, atomWidth+marginSize, atomWidth+marginSize, circlePaint);
+            canvas.drawOval((width-atomWidth)-marginSize, (height-atomWidth)-marginSize, width-marginSize, height-marginSize, circlePaint);
+        } else if (gameField.atomCount() == 3) {
+            canvas.drawOval((width/2)-(atomWidth/2), marginSize, (width/2)+(atomWidth/2), marginSize+atomWidth, circlePaint);
+
+            canvas.drawOval((width-atomWidth)-marginSize, (height-atomWidth)-marginSize, width-marginSize, height-marginSize, circlePaint);
+            canvas.drawOval(marginSize, (height-atomWidth)-marginSize, marginSize + atomWidth, height-marginSize, circlePaint);
+
+        }
+
+
     }
 
     public int getRow() {
